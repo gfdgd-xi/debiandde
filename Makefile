e08a00784ad84c8356f50cd24e68b2c4e100e137
gfdgd_xi <3025613752@qq.com>
2024-02-08 23:14:30 +0800

    修改内容以便支持使用debian installer时能正确配置程序

--- Makefile
+++ Makefile
@@ -4,6 +4,8 @@ build:
 install:
 	mkdir -pv $(DESTDIR)/etc/profile.d/
 	mkdir -pv $(DESTDIR)/etc/apt/sources.list.d/
+	mkdir -pv $(DESTDIR)/usr/lib/systemd/system/
 	cp -rv dde.sh $(DESTDIR)/etc/profile.d/dde.sh
 	cp -rv debian-dde.list $(DESTDIR)/etc/apt/sources.list.d/debian-dde.list
+	cp -rv debiandde-first-config.service $(DESTDIR)/usr/lib/systemd/system/
 	chmod +x $(DESTDIR)/etc/profile.d/dde.sh
--- dde.sh
+++ dde.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 export PATH=$PATH:/sbin
 export QT_QPA_PLATFORMTHEME=deepin
+
+if [[ -f /var/log/installer/lsb-release ]]; then
+    # 使用 Debian Installer 安装系统
+    # 判定是否已经配置过
+    if [[ ! -f /etc/debiandde-config-finished ]]; then
+        # 获取用户名
+        userLineNumber=`grep -n "Name: passwd/username" /var/log/installer/cdebconf/questions.dat | grep -v "e-bad" | grep -v "e-reserved" | cut -d ":" -f 1`
+        userLineNumberEnd=`expr $userLineNumber + 4`
+        userName=`sed -n "$userLineNumber,${userLineNumberEnd}p" /var/log/installer/cdebconf/questions.dat | grep Value:  | cut -d ":" -f 2 | cut -d " " -f 2`
+        usermod -aG sudo "$userName"
+        rm -rfv /etc/apt/sources.list.d/debian.list
+        apt purge deepin-installer -y
+        
+        
+        touch /etc/debiandde-config-finished
+    fi
+fi
